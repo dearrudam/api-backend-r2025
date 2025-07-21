3f8dc7113f473240c81dd2cf7ee991f0e69de02d
Maximillian Arruda <dearrudam@gmail.com>
2025-07-20 22:58:55 -0300

    feat: add filter for payments by creation date
    Implemented a method to filter payments based on their creation date,
    allowing for more flexible querying of payment records.

--- src/main/java/org/acme/domain/Payment.java
+++ src/main/java/org/acme/domain/Payment.java
@@ -2,12 +2,12 @@ package org.acme.domain;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.function.Predicate;
 
 public record Payment(String correlationId,
                       RemotePaymentName processedBy,
                       BigDecimal amount,
                       Instant createAt) {
-
 
     public static Payment of(String correlationId,
                              RemotePaymentName processedBy,
@@ -18,5 +18,17 @@ public record Payment(String correlationId,
                 amount,
                 createAt
         );
+    }
+
+    public static Predicate<Payment> createdOn(Instant from, Instant to) {
+        Predicate<Payment> fromWasOmitted = unused -> from == null;
+        Predicate<Payment> toWasOmitted = unused -> to == null;
+
+        Predicate<Payment> afterOrEqualFrom = payment -> from != null && from.isBefore(payment.createAt()) || from.equals(payment.createAt());
+        Predicate<Payment> beforeOrEqualTo = payment -> to != null && to.isAfter(payment.createAt()) || to.equals(payment.createAt());
+
+        Predicate<Payment> fromTo = fromWasOmitted.or(afterOrEqualFrom)
+                .and(toWasOmitted.or(beforeOrEqualTo));
+        return fromTo;
     }
 }
