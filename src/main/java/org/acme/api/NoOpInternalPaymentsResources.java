07388c4e8a3911babafc833998e87251e2f51462
Maximillian Arruda <dearrudam@gmail.com>
2025-07-20 22:58:05 -0300

    refactor: rename dummy payments resource class
    Renamed DummyInternalPaymentsResources to NoOpInternalPaymentsResources
    and simplified the class by removing unused methods and dependencies.

--- src/main/java/org/acme/api/NoOpInternalPaymentsResources.java
+++ src/main/java/org/acme/api/NoOpInternalPaymentsResources.java
@@ -2,39 +2,24 @@ package org.acme.api;
 
 import io.smallrye.common.annotation.RunOnVirtualThread;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
-import org.acme.domain.PaymentWorker;
 import org.acme.domain.PaymentsSummary;
-import org.eclipse.microprofile.health.HealthCheck;
 
 @ApplicationScoped
 @Path("/no-op")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @RunOnVirtualThread
-public class DummyInternalPaymentsResources {
-
-    private final PaymentWorker paymentWorker;
-
-    @Inject
-    public DummyInternalPaymentsResources(PaymentWorker paymentWorker) {
-        this.paymentWorker = paymentWorker;
-    }
+public class NoOpInternalPaymentsResources {
 
     @GET
     @Path("/internal/payments-summary")
-    public PaymentsSummary getSummary(
-            @QueryParam("from") @DefaultValue("") String fromStr,
-            @QueryParam("to") @DefaultValue("") String toStr) {
+    public PaymentsSummary getSummary() {
         return PaymentsSummary.ZERO;
     }
 
@@ -42,12 +27,6 @@ public class DummyInternalPaymentsResources {
     @POST
     public void purgeInternalPayments() {
         // do nothing
-    }
-
-    @Path("/purge-queue")
-    @DELETE
-    public void purgeQueueWorker() {
-        paymentWorker.purge();
     }
 
     @Path("/q/health/ready")
