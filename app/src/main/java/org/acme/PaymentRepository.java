49ae54956faa4ae590d40871347e49fafbe4c65a
Maximillian Arruda <dearrudam@gmail.com>
2025-08-08 21:17:34 -0300

    feat: refactor PaymentRepository for reactive Redis
    Updated PaymentRepository to use reactive Redis commands,
    improving performance and scalability of payment operations.

--- app/src/main/java/org/acme/PaymentRepository.java
+++ app/src/main/java/org/acme/PaymentRepository.java
@@ -1,46 +1,56 @@
 package org.acme;
 
-import io.quarkus.redis.datasource.RedisDataSource;
-import io.quarkus.redis.datasource.keys.KeyCommands;
-import io.quarkus.redis.datasource.list.ListCommands;
+import io.quarkus.redis.datasource.ReactiveRedisDataSource;
+import io.quarkus.redis.datasource.keys.ReactiveKeyCommands;
+import io.quarkus.redis.datasource.list.ReactiveListCommands;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.unchecked.Unchecked;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static java.util.Optional.ofNullable;
 
 @ApplicationScoped
 public class PaymentRepository {
 
     private final String KEY = "payments";
-    private final KeyCommands<String> key;
-    private final ListCommands<String, Payment> commands;
+    private final ReactiveListCommands<String, Payment> commands;
+    private final ReactiveKeyCommands<String> key;
+
 
     @Inject
-    public PaymentRepository(RedisDataSource ds) {
+    public PaymentRepository(ReactiveRedisDataSource ds) {
         this.commands = ds.list(Payment.class);
         this.key = ds.key();
     }
 
-    public Payment savePayment(Payment payment) {
-        this.commands.lpush(KEY, payment);
-        return payment;
+    public Uni<Payment> savePayment(Payment payment) {
+        return this.commands.lpush(KEY, payment)
+                .onItem().transform(Unchecked.function(count -> {
+                    if (count == null || count <= 0) {
+                        throw new RuntimeException("Failed to save payment");
+                    }
+                    return payment;
+                }));
     }
 
-    public Payment saveAsDefaultPayment(Payment payment) {
+    public Uni<Payment> saveAsDefaultPayment(Payment payment) {
         return savePayment(payment.withType(Payment.PaymentType.DEFAULT));
     }
 
-    public Payment saveAsFallbackPayment(Payment payment) {
+    public Uni<Payment> saveAsFallbackPayment(Payment payment) {
         return savePayment(payment.withType(Payment.PaymentType.FALLBACK));
     }
 
-    public Map<String, Summary> summary(Instant from, Instant to) {
+    public Uni<Map<String, Summary>> summary(Instant from, Instant to) {
 
         Predicate<Payment> filter = payment -> {
             if (from == null && to == null) {
@@ -52,33 +62,31 @@ public class PaymentRepository {
                     (to == null || (to.isAfter(requestedAt) || to.equals(requestedAt)));
         };
 
-        Stream<Payment> stream= this.commands
+        return this.commands
                 .lrange(KEY, 0, -1)
-                .stream()
-                .parallel()
-                .filter(filter);
-
-        // Group a parallel stream by payment type and collect into a summary
-
-        Map<String, Summary> summary = stream
-                .collect(Collectors.groupingBy(
-                        payment -> payment.type().name().toLowerCase(),
-                        Collectors.collectingAndThen(
-                                Collectors.toList(),
-                                payments ->
-                                        new Summary(
-                                                Long.valueOf(payments.size()),
-                                                payments.stream().map(Payment::amount).reduce(BigDecimal.ZERO, BigDecimal::add)
-                                        )
-                        )
-                ));
-        // Ensure all payment types are represented in the summary
-        Arrays.stream(Payment.PaymentType.values())
-                .forEach(type -> summary.putIfAbsent(type.name().toLowerCase(), new Summary(0L, BigDecimal.ZERO)));
-        return summary;
+                .flatMap(data -> {
+                    Map<String, Summary> summary = ofNullable(data)
+                            .stream()
+                            .flatMap(Collection::stream)
+                            .filter(filter)
+                            .collect(Collectors.groupingBy(
+                                    payment -> payment.type().name().toLowerCase(),
+                                    Collectors.collectingAndThen(
+                                            Collectors.toList(),
+                                            payments ->
+                                                    new Summary(
+                                                            Long.valueOf(payments.size()),
+                                                            payments.stream().map(Payment::amount).reduce(BigDecimal.ZERO, BigDecimal::add)
+                                                    )
+                                    )
+                            ));
+                    Arrays.stream(Payment.PaymentType.values())
+                            .forEach(type -> summary.putIfAbsent(type.name().toLowerCase(), new Summary(0L, BigDecimal.ZERO)));
+                    return Uni.createFrom().item(summary);
+                });
     }
 
-    public void deleteAll() {
-        this.key.del(KEY);
+    public Uni<?> deleteAll() {
+        return this.key.del(KEY);
     }
 }
