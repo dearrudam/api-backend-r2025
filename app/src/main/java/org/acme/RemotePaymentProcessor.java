4fa0de6315ba6784c453eac57070551f4461bdfb
Maximillian Arruda <dearrudam@gmail.com>
2025-08-08 21:17:50 -0300

    feat: refactor RemotePaymentProcessor for reactive flow
    Updated RemotePaymentProcessor to use reactive programming
    with Uni for asynchronous processing of payment requests,
    improving performance and scalability.

--- app/src/main/java/org/acme/RemotePaymentProcessor.java
+++ app/src/main/java/org/acme/RemotePaymentProcessor.java
@@ -1,11 +1,11 @@
 package org.acme;
 
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.json.bind.Jsonb;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -30,14 +30,15 @@ public class RemotePaymentProcessor {
     private final Long retriesBeforeFallback;
 
     @Inject
-    public RemotePaymentProcessor(PaymentRepository paymentRepository,
-                                  @ConfigProperty(name = "default.payment.url")
-                                  URI defaultURL,
-                                  @ConfigProperty(name = "fallback.payment.url")
-                                  URI fallbackURL,
-                                  @ConfigProperty(name = "retries.before.fallback")
-                                  Optional<Long> retriesBeforeFallback,
-                                  Jsonb jsonb) {
+    public RemotePaymentProcessor(
+            PaymentRepository paymentRepository,
+            @ConfigProperty(name = "default.payment.url")
+            URI defaultURL,
+            @ConfigProperty(name = "fallback.payment.url")
+            URI fallbackURL,
+            @ConfigProperty(name = "retries.before.fallback")
+            Optional<Long> retriesBeforeFallback,
+            Jsonb jsonb) {
         this.paymentRepository = paymentRepository;
         this.defaultURL = defaultURL;
         this.fallbackURL = fallbackURL;
@@ -48,37 +49,31 @@ public class RemotePaymentProcessor {
         this.httpClient = builHttpClient();
     }
 
-    public void process(PaymentRequest paymentRequest) {
+    public Uni<?> process(PaymentRequest paymentRequest) {
         Payment payment = Payment.create(paymentRequest);
-        try {
-            var request = createRequest(defaultURL, payment);
-            var response = httpClient.send(request, HttpResponse.BodyHandlers.discarding());
-            if (response.statusCode() == 200) {
-                paymentRepository.saveAsDefaultPayment(payment);
-                defaultErrorCount.remove(payment.correlationId());
-            } else {
-                if (response.statusCode() == 500) {
-                    long errors = defaultErrorCount
-                            .computeIfAbsent(payment.correlationId(), k -> new AtomicLong(0))
-                            .incrementAndGet();
-                    if (errors >= retriesBeforeFallback) {
-                        processFallback(payment);
-                        defaultErrorCount.remove(payment.correlationId());
-                    } else {
-                        throw new RuntimeException(
-                                "payment processing on the default processor failed with status code: %s - correlationId: %s - attempts: %s"
-                                .formatted(response.statusCode(), payment.correlationId(), errors));
-                    }
-                }
-                throw new UnsupportedOperationException("payment processing on the default processor failed with status code: " + response.statusCode());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new UnsupportedOperationException(e);
-        } finally {
-            sleepIfNeed(payment);
-        }
+        return Uni.createFrom().completionStage(
+                httpClient.sendAsync(createRequest(defaultURL, payment), HttpResponse.BodyHandlers.ofString())
+                        .thenApply(response -> {
+                            if (response.statusCode() == 200) {
+                                return paymentRepository.saveAsDefaultPayment(payment)
+                                        .onItem()
+                                        .invoke(() -> defaultErrorCount.remove(payment.correlationId())).await().indefinitely();
+                            } else {
+                                if (response.statusCode() == 500) {
+                                    long errors = defaultErrorCount
+                                            .computeIfAbsent(payment.correlationId(), k -> new AtomicLong(0))
+                                            .incrementAndGet();
+                                    if (errors >= retriesBeforeFallback) {
+                                        return processFallback(payment).await().indefinitely();
+                                    } else {
+                                        throw new RuntimeException(
+                                                "payment processing on the default processor failed with status code: %s - correlationId: %s - attempts: %s"
+                                                        .formatted(response.statusCode(), payment.correlationId(), errors));
+                                    }
+                                }
+                                return null;
+                            }
+                        }));
     }
 
     private void sleepIfNeed(Payment payment) {
@@ -96,35 +91,38 @@ public class RemotePaymentProcessor {
     }
 
 
-    private void processFallback(Payment payment) throws IOException, InterruptedException {
+    private Uni<?> processFallback(Payment payment) {
         System.out.printf("Processing fallback payment for correlationId %s%n", payment.correlationId());
-        // Fallback to the secondary URL
-        var request = createRequest(fallbackURL, payment);
-        var response = httpClient.send(request, HttpResponse.BodyHandlers.discarding());
-        if (response.statusCode() == 200) {
-            paymentRepository.saveAsFallbackPayment(payment);
-        } else {
-            long errors = fallbackErrorCount
-                    .computeIfAbsent(payment.correlationId(), k -> new AtomicLong(0))
-                    .incrementAndGet();
-            throw new RuntimeException("payment processing on the fallback processor failed with status code: %s - correlationId: %s - attempts: %s"
-                    .formatted(response.statusCode(), payment.correlationId(), errors));
-        }
+        return Uni.createFrom().completionStage(
+                httpClient.sendAsync(createRequest(fallbackURL, payment), HttpResponse.BodyHandlers.ofString())
+                        .thenApply(response -> {
+                            if (response.statusCode() == 200) {
+                                return paymentRepository.saveAsFallbackPayment(payment)
+                                        .onItem()
+                                        .invoke(() -> fallbackErrorCount.remove(payment.correlationId()));
+                            } else {
+                                long errors = fallbackErrorCount
+                                        .computeIfAbsent(payment.correlationId(), k -> new AtomicLong(0))
+                                        .incrementAndGet();
+                                throw new RuntimeException("payment processing on the fallback processor failed with status code: %s - correlationId: %s - attempts: %s"
+                                        .formatted(response.statusCode(), payment.correlationId(), errors));
+                            }
+                        }));
     }
 
     private HttpRequest createRequest(URI defaultURL, Payment payment) {
+        String json = jsonb.toJson(payment);
         return HttpRequest.newBuilder(defaultURL)
                 .header("Content-Type", "application/json")
                 .timeout(ofSeconds(5))
-                .POST(HttpRequest.BodyPublishers.ofString(jsonb.toJson(payment)))
+                .POST(HttpRequest.BodyPublishers.ofString(json))
                 .build();
     }
 
-    private static HttpClient builHttpClient() {
+    private HttpClient builHttpClient() {
         return HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.NEVER)
                 .version(HttpClient.Version.HTTP_1_1)
-                .executor(Runnable::run)
                 .build();
     }
 }
