365d71517f4d6594907b6d8500427c65e62f1b00
Maximillian Arruda <dearrudam@gmail.com>
2025-08-09 21:16:54 -0300

    feat: update Payment record and filter logic
    Refactored the Payment record to change the 'createAt' field to 'requestedAt'
    and simplified the createdOn predicate logic for better readability and maintainability.

--- in-memory-quarkus/src/main/java/org/acme/domain/Payment.java
+++ in-memory-quarkus/src/main/java/org/acme/domain/Payment.java
@@ -1,35 +1,24 @@
 package org.acme.domain;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.Instant;
 import java.util.function.Predicate;
 
-public record Payment(String correlationId,
-                      RemotePaymentName processedBy,
-                      BigDecimal amount,
-                      Instant createAt) {
+public record Payment(String correlationId, RemotePaymentName processedBy, BigDecimal amount, Instant requestedAt) {
 
-    public static Payment of(String correlationId,
-                             RemotePaymentName processedBy,
-                             BigDecimal amount,
-                             Instant createAt) {
-        return new Payment(correlationId,
-                processedBy,
-                amount.setScale(2, RoundingMode.HALF_DOWN),
-                createAt
-        );
+    public static Payment of(String correlationId, RemotePaymentName processedBy, BigDecimal amount, Instant createAt) {
+        return new Payment(correlationId, processedBy, amount, createAt);
     }
 
     public static Predicate<Payment> createdOn(Instant from, Instant to) {
-        Predicate<Payment> fromWasOmitted = unused -> from == null;
-        Predicate<Payment> toWasOmitted = unused -> to == null;
-
-        Predicate<Payment> afterOrEqualFrom = payment -> from != null && from.isBefore(payment.createAt()) || from.equals(payment.createAt());
-        Predicate<Payment> beforeOrEqualTo = payment -> to != null && to.isAfter(payment.createAt()) || to.equals(payment.createAt());
-
-        Predicate<Payment> fromTo = fromWasOmitted.or(afterOrEqualFrom)
-                .and(toWasOmitted.or(beforeOrEqualTo));
-        return fromTo;
+        return payment -> {
+            if (from == null && to == null) {
+                return true;
+            }
+            Instant requestedAt = payment.requestedAt();
+            return (from == null || (from.isBefore(requestedAt) || from.equals(requestedAt)))
+                    &&
+                    (to == null || (to.isAfter(requestedAt) || to.equals(requestedAt)));
+        };
     }
 }
